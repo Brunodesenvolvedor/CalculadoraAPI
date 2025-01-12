bf6342ee808331d5cd831c6f4e2c490dea5aa7a0
bruno <emailprofissionaldobruno@gmail.com>
2025-01-12 12:44:34 -0300

    ajustando css

--- obj/Debug/net8.0/Calculadora API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculadora API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c746ba83d32d979aeb6364e459f4e036e391a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50816a081e6d625a4e747b314edfe26e4a65bfd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
